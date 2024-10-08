3a8fc29ee0a01547b5f686edb66a8e224eecda03
piyush-sharma01 <piyushdadhich5445@gmail.com>
2024-10-08 15:37:41 +0530

    new

---
+++ java script/calculater.js
@@ -0,0 +1,29 @@
+// function add()
+// {
+// var a=document.getElementById("1").Value;
+// var b=document.getElementById("2").Value;
+// document.write("calculation is"+a+b);
+
+// }
+function sub()
+{
+var a=document.getElementById("1").Value;
+var b=document.getElementById("2").Value;
+var c=a-b;
+document.write("calculation is"+c);
+}
+
+// function div()
+// {
+// var a=document.getElementById("1").Value;
+// var b=document.getElementById("2").Value;
+// document.write("calculation is"+a+b);
+
+// }
+// function add()
+// {
+// var a=document.getElementById("1").Value;
+// var b=document.getElementById("2").Value;
+// document.write("calculation is"+a+b);
+
+// }
