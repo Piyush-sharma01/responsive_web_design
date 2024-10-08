e02537fa22fb3dfc13fedc69b4d0041f3a083ce3
piyush-sharma01 <piyushdadhich5445@gmail.com>
2024-10-08 15:23:14 +0530

    new code

--- java script/formclick.js
+++ java script/formclick.js
@@ -1,8 +1,21 @@
 function square()
 {
- var r=document.getElementById(s).value;
- result= r*r;
-document.write("<h2>"+r+"squre is"+result+"</h2>");
+//  var r=document.getElementById("s").value;
+ 
+// document.write("<h2>"+r+"squre is"+r*r+"</h2>");
+
+// var r=document.getElementsByName("s1")[0].value;
+//  if(isNaN(r))
+//     alert("please enter a valid number");
+// else{
+// document.write("<h2>"+r+"squre is"+r*r+"</h2>");
+// }
 
 
+var r=document.getElementsBytagName("input").value;
+ if(isNaN(r))
+    alert("please enter a valid number");
+else{
+document.write("<h2>"+r+"squre is"+r*r+"</h2>");
+}
 }
