37633c619bdb92e471c28798532b24d903419d8e
piyush-sharma01 <piyushdadhich5445@gmail.com>
2024-10-10 15:51:54 +0530

    claculater

--- java script/calculater.js
+++ java script/calculater.js
@@ -1,29 +1,9 @@
-// function add()
-// {
-// var a=document.getElementById("1").Value;
-// var b=document.getElementById("2").Value;
-// document.write("calculation is"+a+b);
 
-// }
-function sub()
-{
-var a=document.getElementById("1").Value;
-var b=document.getElementById("2").Value;
-var c=a-b;
-document.write("calculation is"+c);
+function add() {
+    var b = parseInt(document.getElementById("s").value);
+        
+
+        var c = b + b; // This will double the input
+        document.write(c);
+    
 }
-
-// function div()
-// {
-// var a=document.getElementById("1").Value;
-// var b=document.getElementById("2").Value;
-// document.write("calculation is"+a+b);
-
-// }
-// function add()
-// {
-// var a=document.getElementById("1").Value;
-// var b=document.getElementById("2").Value;
-// document.write("calculation is"+a+b);
-
-// }
